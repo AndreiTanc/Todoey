447ee291dff3ef11389b2ab1a3376833d2f89fd3
Andrei <tancandrei001@gmail.com>
2019-05-13 00:09:21 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Andrei Tanc on 13/05/2019.
+//  Copyright © 2019 Andrei Tanc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
